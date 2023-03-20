b957660e9970b99533136954e26ce0edebb9a94c
Devfrank94 <francescomurro94@gmail.com>
2023-03-20 18:02:05 +0100

    add condition discount for age

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ let prTicket = Math.round((numbKm * 0.21)*100)/100;
 document.getElementById('number-km').innerHTML= numbKm;
 document.getElementById('age-passenger').innerHTML= age;
 
+/*Condizione per il calcolo dei biglietti scontati, altrimenti se l'utente
+  non rientra nei parametri gli verrà applicato il prezzo normale. 
+*/
+if (age < 18) {
+  discount20 = (prTicket * 20) / 100;
+  prTicketDisc20 = prTicket - discount20;
+  prTicketDisc20 = prTicketDisc20.toFixed(2);
+  document.getElementById('discount-20').innerHTML = prTicketDisc20 + '€';
+
+}else if (age > 65) {
+  discount40 = (prTicket * 40) / 100;
+  prTicketDisc40 = prTicket - discount40;
+  prTicketDisc40 = prTicketDisc40.toFixed(2);
+  document.getElementById('discount-40').innerHTML = prTicketDisc40 + '€';
+
+}else{
+  prTicket = prTicket.toFixed(2);
+  document.getElementById('price-ticket').innerHTML = prTicket + '€';
+}

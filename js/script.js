0916080eb7c7e7dff0aeb56bdd661454009cd43f
Devfrank94 <francescomurro94@gmail.com>
2023-03-20 17:04:47 +0100

    calc cost/Km

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Dichiarazione e assegnazione delle variabili
+let numbKm = parseInt(prompt('Inserire km da percorrere'));
+
+let age = parseInt(prompt('Inserire età del passeggero'));
+
+//Calcolo prezzo del biglietto con 2 decimali dopo virgola
+let prTicket = Math.round((numbKm * 0.21)*100)/100;
